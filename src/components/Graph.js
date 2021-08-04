f3a731e75d95baecb18c4b72e23c63dcf13efc7d
Herman Liu <hermanliu95@gmail.com>
2021-08-03 18:45:23 -0700

    complete

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,12 +2,12 @@ import React from "react";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-const yAxis = ["0", "500", "1,500", "2,000", "2,500", "3,000", "3,500"]
+const yAxis = ["0", "500", "1,000", "1,500", "2,000", "2,500", "3,000", "3,500"]
 const xAxis = ["$1M", "$1.5M", "$2M", "$2.5M", "$3M", "$3.5M", "$4M", "$4.5M", "$5M"]
+
 
 const ChartContainer = () => (
   <>
-  
     <div className="chart-container">
       <ArrowBackIosIcon className="top-arrow"/>
       <ArrowForwardIosIcon />
@@ -16,11 +16,24 @@ const ChartContainer = () => (
           xAxis.map((x) => <p> {x} </p>)
         }
       </div>
-
       <div className="y-axis">
         {
           yAxis.map((y) => <p> {y} </p>)
         }
+      </div>
+
+      <div className="graph-center"></div>
+
+      {/* Create the Bubble Data  */}
+      <div className="top-bubble-set">
+        <p className="bubble one">Affinity Dental</p>
+        <p className="bubble two">Bugs Bunny Dental</p>
+        <p className="bubble three">Awesome Smiles</p>
+      </div>
+      <div className="bottom-bubble-set">
+        <p className="bubble four">Ferengi Teeth</p>
+        <p className="bubble five">Klingon Smiles</p>
+        <p className="bubble six">Bluetooth Dental</p>
       </div>
     </div>
   </>
